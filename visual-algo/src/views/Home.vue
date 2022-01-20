3a12284e43e450cd71ef287df5c7e0df2a46081f
GregoryKogan <grisha.koganovskiy@gmail.com>
2022-01-20 21:02:02 +0300

    Vuetify added

--- visual-algo/src/views/Home.vue
+++ visual-algo/src/views/Home.vue
@@ -1,16 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <hello-world />
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Vue from "vue";
+import HelloWorld from "../components/HelloWorld.vue";
 
-export default defineComponent({
+export default Vue.extend({
   name: "Home",
+
   components: {
     HelloWorld,
   },
